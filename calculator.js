63c04266069e0cac79bbd4185c49d224358fbade
hahmatovic1 <72711477+hahmatovic1@users.noreply.github.com>
2022-09-14 15:08:04 +0200

    feat: Require body-parser

--- calculator.js
+++ calculator.js
@@ -1,12 +1,16 @@
 const express = require("express");
+const bodyParser = require("body-parser")
+
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+
 
 app.get("/", function(req,res){
   res.sendFile(__dirname +   "/index.html");
 })
 
 app.post("/", function(req, res){
-  console.log(req);
+  console.log(req.body)
 })
 
 app.listen(3000, function(){
