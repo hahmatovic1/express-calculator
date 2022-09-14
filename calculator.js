93bd2e8e85e9f600127a3128ef2dd8169dbfbbef
hahmatovic1 <72711477+hahmatovic1@users.noreply.github.com>
2022-09-14 14:28:30 +0200

    feat: Transfer index.html to root path

--- calculator.js
+++ calculator.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req,res){
-  res.send(`<h1>My calculator</h1>`); 
+  res.sendFile(__dirname +   "/index.html");
 })
 
 app.listen(3000, function(){
