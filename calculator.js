836bc2c0bcee715b86118a87050c3bec39005186
hahmatovic1 <72711477+hahmatovic1@users.noreply.github.com>
2022-09-14 14:50:07 +0200

    feat: Post method displays request

--- calculator.js
+++ calculator.js
@@ -5,6 +5,10 @@ app.get("/", function(req,res){
   res.sendFile(__dirname +   "/index.html");
 })
 
+app.post("/", function(req, res){
+  console.log(req);
+})
+
 app.listen(3000, function(){
   console.log("Server is running on port 3000")
 })
